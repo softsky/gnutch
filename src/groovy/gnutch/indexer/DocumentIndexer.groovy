5f4c79ee20efc4c5eba545b928bf4529d4c5f3ca
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2013-09-26 15:52:12 +0300

    improved access to TransformerFactory through use of ThreadLocal variable. Added trace 'gnutch.indexer.DocumentIndexer.dom configuration option

--- src/groovy/gnutch/indexer/DocumentIndexer.groovy
+++ src/groovy/gnutch/indexer/DocumentIndexer.groovy
@@ -1,56 +1,82 @@
 package gnutch.indexer
 
-import javax.xml.transform.TransformerFactory
-import javax.xml.transform.dom.DOMResult
-import javax.xml.transform.dom.DOMSource
-
-import org.apache.camel.Body
 import org.apache.camel.Header
-import org.apache.commons.logging.LogFactory
+import org.apache.camel.Body
+
+import java.util.Collections
+
 import org.w3c.dom.Document
+
+import org.springframework.core.io.ClassPathResource
+
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.dom.DOMResult
+
+import javax.xml.transform.stream.StreamSource
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.Transformer
+
+import org.apache.commons.logging.LogFactory
 
 import javax.xml.transform.stream.StreamResult
 
 class DocumentIndexer {
-  private static log = LogFactory.getLog(this)
+  private static def log = LogFactory.getLog(this)
   /** Map of regexp/stylesheet values */
-  final Map<String, Document> transformations = Collections.synchronizedMap([:])
-  private static final String TRANSFORMER_FACTORY_CLASS = "org.apache.xalan.processor.TransformerFactoryImpl"
+  final def Map<String, Document> transformations = Collections.synchronizedMap([:])
+  private static final String TRANSFORMER_FACTORY_CLASS = "org.apache.xalan.processor.TransformerFactoryImpl";
 
-  Document index(@Header("contextURI") String contextURI, @Body Document body){
+  private static final ThreadLocal<TransformerFactory> transformerFactoryLocal =
+    new ThreadLocal<TransformerFactory>() {
+      @Override 
+      protected TransformerFactory initialValue() {
+        System.out.println("Creating initial value of TransformerFactory")
+        return  TransformerFactory.newInstance(TRANSFORMER_FACTORY_CLASS, null);
+      }
+    };
+  
+  public Document index(@Header("contextURI") String contextURI, @Body Document body){
     log.trace "Testing ${contextURI}, ${body}"
-    def matched
-    def result
+    def matched = null
+    def result 
     synchronized(transformations) {
-      transformations.each { entry ->
-        // looking for one that match
-        if(contextURI.matches(entry.key)){
-          if(matched) throw new RuntimeException("""contextURI:${contextURI} already matched ${matched} pattern\n
+    transformations.each { entry ->
+      // looking for one that match
+      if(contextURI.matches(entry.key)){
+        if(matched) throw new RuntimeException("""contextURI:${contextURI} already matched ${matched} pattern\n
 Now it also matches ${entry.key} pattern, which significantly drops perfromace\n
 as trasnformation is executed couple of times. Please, check your transformations map.
 """)
-          matched = entry.key
-          def domResult = new DOMResult()
-          def tf = TransformerFactory.newInstance(TRANSFORMER_FACTORY_CLASS, null)
-          def transformer = tf.newTransformer(new DOMSource(entry.value))
+        matched = entry.key
+        def domResult = new DOMResult()
+        TransformerFactory tf = transformerFactoryLocal.get()
+        def transformer = tf.newTransformer(new DOMSource(entry.value))
 
-          if(transformer == null) {
-            throw new RuntimeException("XLT is seems invalid for this URI: ${contextURI}")
-          }
+        if(transformer == null)
+          throw new RuntimeException("XSLT is seems invalid for this URI: ${contextURI}")
 
-          transformer.setParameter('contextURI', contextURI)
-          transformer.transform(new DOMSource(body), domResult)
-          result = domResult.node
+        transformer.setParameter('contextURI', contextURI)
+        transformer.transform(new DOMSource(body), domResult)
+
+        // if TRACE log level is enabled for gnutch.indexer.DocumentIndexer.dom
+        // dumping input XHTML onto System.out
+        if(LogFactory.getLog(this.getClass().name + ".dom").isTraceEnabled()){
+          def t = tf.newTransformer()
+          t.transform(new DOMSource(body), new StreamResult(System.out))
         }
+
+        result = domResult.node
       }
+    }
     }
     log.trace "${contextURI} ${result?' is indexable':' is not indexable'}"
     return result
   }
 
-  boolean isIndexable(@Header("contextURI") String contextURI){
+  public boolean isIndexable(@Header("contextURI") String contextURI){
     synchronized(transformations){
       return transformations.keySet().any { contextURI.matches(it) }
     }
   }
+
 }
