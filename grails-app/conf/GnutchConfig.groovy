31d0e3a43b3a6c1b1455c5c4de0f77f6d3b752e9
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2013-09-30 17:23:42 +0300

    GnutchConfig minimalized

--- grails-app/conf/GnutchConfig.groovy
+++ grails-app/conf/GnutchConfig.groovy
@@ -21,52 +21,13 @@ gnutch {
     // org.apache.camel.Processor definition. Called after XML is built
     // ex.in.body contains XML document (actually contains reference to org.w3c.org.Document)
     postXML = { Exchange ex ->
-      // aquiring `date` field
-      def doc = ex.in.body
-      def dateNode = doc.createElement("field")
-      dateNode.setAttribute("name", "date")
-      def format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
-      dateNode.appendChild(doc.createTextNode(format.format(new Date())))
-      doc.firstChild.appendChild(dateNode)
-
-      // aquiring `source` field
-      def nodeList = doc.getElementsByTagName("field")
-      def id = nodeList.find { it.getAttribute("name") == "id"}.textContent
-    
-      def m = id =~ /https?:\/\/([^\/]*)\//
-      try{
-        def sourceElement = doc.createElement("field")
-        sourceElement.setAttribute("name", "source")
-        sourceElement.appendChild(doc.createTextNode(m[0][1]))
-        doc.firstChild.appendChild(sourceElement)
-      }catch(Exception e){
-        println id + " no match found"
-      }
     } 
 
     validate = { Exchange ex ->
-      def title = XPathAPI.eval(ex.in.body, "//field[@name = 'title']").str()
-      def content = XPathAPI.eval(ex.in.body, "//field[@name = 'content']").str()
-
-      return (title.equals("") == false) && (content.equals("") == false)
     }
 
     // Route definition. Should consume from 'direct:publish' and provide some business logic
     publish = {
-       // This route will save files to a directory
-      /*
-       from('direct:publish').
-       to("file:///home/archer/tmp/gnutch-output/")
-      */
-
-
-      // This route will commit to a solr server
-      from('direct:publish').
-      setHeader(Exchange.HTTP_URI, constant("http://softsky.com.ua/solr/collection1/update?commit=true")).
-      setHeader(Exchange.HTTP_METHOD, constant('POST')).
-      setHeader(Exchange.CONTENT_TYPE, constant('application/xml')).
-      to("http://null")
-
     }
 
   }
@@ -74,28 +35,17 @@ gnutch {
   http {
     // UserAgent string. Better if contain email address of person who is responsible 
     // for crawling. That will allow source owners to contact person directly
-    userAgent = "GNutch crawler. Contact maintainer: admin@softsky.com.ua"
+    userAgent = "GNutch crawler (https://github.com/softsky/gnutch): admin@gnutch.org"
     // Maximmum number of connections per host
     defaultMaxConnectionsPerHost = 1000
     // Maximmum number of total connections
     maxTotalConnections = 1000
   }
-
-  solr {
-    // URL to Solr server
-    masterCoreUrl = 'http://softsky.com.ua/solr/collection1'
-    slaveCoreUrl = 'http://softsky.com.ua/solr/collection1'
-  }
- 
   
   activemq {
     // URL to message broker
     brokerURL = 'vm://localhost'
     // brokerURL = 'tcp://localhost:61616'
     // conf = 'classpath:activemq.xml'
-  } 
-  
-  support.email = 'sourcingtool-support@softsky.com.ua'
-  validation.query = '-title:["" TO *]&group=true&group.field=source'
-
+  }   
 }
