f7af8e1d8750aed1c486682835fcb72b95ca4f15
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2013-12-27 13:52:13 +0200

    RegexUrlChecker is now made public

--- src/java/gnutch/urls/RegexUrlChecker.java
+++ src/java/gnutch/urls/RegexUrlChecker.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.Log;
 
-class RegexUrlChecker {
+public class RegexUrlChecker {
     @Autowired PatternService patternService;
 
     private static final ThreadLocal<RegexUrlChecker> checkerLocal =
