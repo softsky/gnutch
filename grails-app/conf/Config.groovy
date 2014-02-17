ab012dc5a2a3da50976d0bbd4f249e874e5511ce
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2014-02-17 07:48:12 +0200

    test environment optimized

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -3,19 +3,6 @@ import org.apache.xpath.XPathAPI
 import java.text.SimpleDateFormat
 import org.apache.camel.LoggingLevel
 
-log4j = {
-   error  'org.codehaus.groovy.grails',
-          'org.springframework',
-          'org.hibernate',
-          'net.sf.ehcache.hibernate'
-
-   warn  'org.apache.camel'
-   trace  'gnutch'
-
-   //trace 'gnutch.indexer.DocumentIndexer.dom'
-}
-
-
 gnutch {
   // Input route definition
   inputRoute = 'file:///home/archer/tmp/gnutch-input'
@@ -161,59 +148,25 @@ environments {
   }
 
   test {
+    log4j = {
+      error  'org.codehaus.groovy.grails',
+      'org.springframework',
+      'org.hibernate',
+      'net.sf.ehcache.hibernate'
+
+      warn  'org.apache.camel'
+      debug 'gnutch'
+    }
+
     gnutch {
-      // Input route definition
-      inputRoute = 'file:///home/archer/tmp/gnutch-input'
+      crawl.threads = {-> java.lang.Runtime.getRuntime().availableProcessors() }()
+      crawl.multiplier = 5
 
-      aggregationTime = 10000L
-
-      crawl {
-        // Size of crawling thread pool
-        threads = {-> java.lang.Runtime.getRuntime().availableProcessors() }()
-        multiplier = 5
+      gnutch.handlers.publish = {
+        from('direct:publish').
+        log(LoggingLevel.DEBUG, 'gnutch', 'Committed')
       }
-
-      handlers {
-        // org.apache.camel.Processor definition. Called after HTML is transformed into XHTML
-        // ex.in.body contains XHML document (actually contains reference to org.w3c.org.Document)
-        postXHTML = { Exchange ex ->
-
-        }
-
-        // org.apache.camel.Processor definition. Called after XML is built
-        // ex.in.body contains XML document (actually contains reference to org.w3c.org.Document)
-        postXML = { Exchange ex ->
-
-        }
-
-        validate = { Exchange ex ->
-          return true
-        }
-
-        // Route definition. Should consume from 'direct:publish' and provide some business logic
-        publish = {
-          from('direct:publish').
-          log(LoggingLevel.DEBUG, 'gnutch', 'Committed')
-        }
-
-      }
-
-      http {
-        // UserAgent string. Better if contain email address of person who is responsible
-        // for crawling. That will allow source owners to contact person directly
-        userAgent = "GNutch crawler (https://github.com/softsky/gnutch): admin@gnutch.org"
-        // Maximmum number of connections per host
-        defaultMaxConnectionsPerHost = 1000
-        // Maximmum number of total connections
-        maxTotalConnections = 1000
-      }
-
-      activemq {
-        // URL to message broker
-        brokerURL = 'vm://localhost?broker.persistent=false'
-        // brokerURL = 'tcp://localhost:61616'
-        // conf = 'classpath:activemq.xml'
-      }
+      gnutch.activemq.brokerURL = 'vm://localhost?broker.persistent=false'
     }
 
   }
