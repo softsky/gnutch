b311a8cdfef903fd59c6e7354a722e3e3e792512
wasim <wasim.akhtar@ongraph.com>
2014-02-20 12:23:24 +0530

    Formatting changes done #15.

--- test/integration/gnutch/RouteStressTests.groovy
+++ test/integration/gnutch/RouteStressTests.groovy
@@ -30,113 +30,116 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 
 class RouteStressTests extends CamelTestSupport {
-    def grailsApplication
-    def config
+  def grailsApplication
+  def config
 
-    ProducerTemplate producerTemplate;
-    CamelContext camelContext
+  ProducerTemplate producerTemplate;
+  CamelContext camelContext
 
-    def jmsConnectionFactory
+  def jmsConnectionFactory
 
-    private Server server  // jetty server
+  private Server server  // jetty server
 
-    protected CamelContext createCamelContext() throws Exception {
-        return camelContext
-    }
+  protected CamelContext createCamelContext() throws Exception {
+    return camelContext
+  }
 
-    @Before
-    void setUp() {
-        super.setUp()
+  @Before
+  void setUp(){
+    super.setUp()
 
-        config = grailsApplication?.config
+    config = grailsApplication?.config
 
     camelContext.start() // starting camel ourselves
 
-        camelContext.shutdownStrategy.timeout = 60 // setting shutdown timeout to 1 minute (60 seconds)
+    camelContext.shutdownStrategy.timeout = 60 // setting shutdown timeout to 1 minute (60 seconds)
 
-        // Running embedded Jetty server to crawl from. Application sits in test/integation/resources/web-app
-        server = new Server(8080);
-        ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.directoriesListed = true;
-        resource_handler.welcomeFiles = ["index.html"];
+    // Running embedded Jetty server to crawl from. Application sits in test/integation/resources/web-app
+    server = new Server(8080);
+    ResourceHandler resource_handler = new ResourceHandler();
+    resource_handler.directoriesListed = true;
+    resource_handler.welcomeFiles = ["index.html"];
 
-        resource_handler.resourceBase = "test/integration/resources/web-app";
+    resource_handler.resourceBase = "test/integration/resources/web-app";
+          
+    HandlerList handlers = new HandlerList();
+    handlers.handlers = [resource_handler, new DefaultHandler()];
+    server.handler = handlers;
+    server.start();
+  }
 
-        HandlerList handlers = new HandlerList();
-        handlers.handlers = [resource_handler, new DefaultHandler()];
-        server.handler = handlers;
-        server.start();
+  @After
+  void tearDown(){
+    camelContext.stop() // stopping camel ourselves and after stop wiping out activemq queue
+
+    server.stop();   
+ 
+    super.tearDown();
+  }
+
+  @Test
+  @DirtiesContext
+  void testStress() {
+    camelContext.
+    getRouteDefinition('aggregation').
+    adviceWith(camelContext,
+               new AdviceWithRouteBuilder() { 
+                 @Override
+                 public void configure() throws Exception {
+                   mockEndpointsAndSkip("direct:publish")
+                 }
+               });
+    def xsltDir = new ClassPathResource('xslt').file
+    xsltDir.eachFile { file ->
+      def destFile = new File(grailsApplication.config.gnutch.inputRoute.replace('file://', '') + '/' + file.name)
+      destFile.append(file.newInputStream())
     }
 
-    @After
-    void tearDown() {
-        camelContext.stop() // stopping camel ourselves and after stop wiping out activemq queue
+    def mockEndpoint = getMockEndpoint("mock:direct:publish")
 
-        server.stop();
+    mockEndpoint.expectedMinimumMessageCount(1) // expecting some messages
+    def expectation = {-> 
+      def ex = receivedExchanges[0]
+      assert ex.in.body.documentElement.nodeName == 'add'
+      assert ex.in.body.getElementsByTagName('doc').length > 0
+      println "Commit happened"
+    } as Runnable
+    expectation.delegate = mockEndpoint
+    mockEndpoint.expects(expectation)
 
-        super.tearDown();
-    }
+//    assertMockEndpointsSatisfied(config.gnutch.aggregationTime + 5, TimeUnit.SECONDS) // let this test work for 15 seconds
+    assertMockEndpointsSatisfied(grailsApplication.config.gnutch.aggregationTime + 10, TimeUnit.SECONDS)
+  }
 
-    @Test
-    @DirtiesContext
-    void testStress() {
-        camelContext.
-                getRouteDefinition('aggregation').
-                adviceWith(camelContext,
-                        new AdviceWithRouteBuilder() {
-                            @Override
-                            public void configure() throws Exception {
-                                mockEndpointsAndSkip("direct:publish")
-                            }
-                        });
-        def xsltDir = new ClassPathResource('xslt').file
-        xsltDir.eachFile { file ->
-            def destFile = new File(grailsApplication.config.gnutch.inputRoute.replace('file://', '') + '/' + file.name)
-            destFile.append(file.newInputStream())
-        }
-        def mockEndpoint = getMockEndpoint("mock:direct:publish")
-        mockEndpoint.expectedMinimumMessageCount(1) // expecting some messages
-        def expectation = { ->
-            def ex = receivedExchanges[0]
-            assert ex.in.body.documentElement.nodeName == 'add'
-            assert ex.in.body.getElementsByTagName('doc').length > 0
-        } as Runnable
-        expectation.delegate = mockEndpoint
-        mockEndpoint.expects(expectation)
-        assertMockEndpointsSatisfied(grailsApplication.config.gnutch.aggregationTime + 10, TimeUnit.SECONDS)
-//        assertMockEndpointsSatisfied(15, TimeUnit.SECONDS)
-        // let this test work for 15 seconds
-    }
+  @Test
+  @DirtiesContext
+  void testFullCycle() {
+    camelContext.
+    getRouteDefinition('aggregation').
+    adviceWith(camelContext,
+               new AdviceWithRouteBuilder() { 
+                 @Override
+                 public void configure() throws Exception {
+                   mockEndpointsAndSkip("direct:publish")
+                 }
+               });
+    def mockEndpoint = getMockEndpoint("mock:direct:publish")
 
-    @Test
-    @DirtiesContext
-    void testFullCycle() {
-        camelContext.
-                getRouteDefinition('aggregation').
-                adviceWith(camelContext,
-                        new AdviceWithRouteBuilder() {
-                            @Override
-                            public void configure() throws Exception {
-                                mockEndpointsAndSkip("direct:publish")
-                            }
-                        });
-        def mockEndpoint = getMockEndpoint("mock:direct:publish")
+    mockEndpoint.expectedMessageCount(1)
+    def expectation = {-> 
+      def ex = receivedExchanges[0]
+      assert ex.in.body.documentElement.nodeName == 'add'
+      assert ex.in.body.getElementsByTagName('doc').length > 0
+      
+    } as Runnable
+    expectation.delegate = mockEndpoint
+    mockEndpoint.expects(expectation)
 
-        mockEndpoint.expectedMessageCount(1)
-        def expectation = { ->
-            def ex = receivedExchanges[0]
-            assert ex.in.body.documentElement.nodeName == 'add'
-            assert ex.in.body.getElementsByTagName('doc').length > 0
+    // saving file
+    def resourceStream = new ClassPathResource('xslt/localhost.xsl').inputStream
+    def destFile = new File(grailsApplication.config.gnutch.inputRoute.replace('file://', '') + '/localhost.xsl')
+    destFile.append(resourceStream)
 
-        } as Runnable
-        expectation.delegate = mockEndpoint
-        mockEndpoint.expects(expectation)
-
-        // saving file
-        def resourceStream = new ClassPathResource('xslt/localhost.xsl').inputStream
-        def destFile = new File(grailsApplication.config.gnutch.inputRoute.replace('file://', '') + '/localhost.xsl')
-        destFile.append(resourceStream)
-
-        assertMockEndpointsSatisfied(config.gnutch.aggregationTime + 5, TimeUnit.SECONDS)
-    }
+    assertMockEndpointsSatisfied(config.gnutch.aggregationTime + 5, TimeUnit.SECONDS)
+  }
 }
--- test/integration/gnutch/RouteTests.groovy
+++ test/integration/gnutch/RouteTests.groovy
@@ -33,110 +33,114 @@ import org.eclipse.jetty.server.handler.ResourceHandler;
 
 class RouteTests extends CamelTestSupport {
 
-    def grailsApplication
+  def grailsApplication
 
-    def config
+  def config
 
-    ProducerTemplate producerTemplate;
-    CamelContext camelContext
+  ProducerTemplate producerTemplate;
+  CamelContext camelContext
 
-    def jmsConnectionFactory
+  def jmsConnectionFactory
 
-    private Server server  // jetty server
+  private Server server  // jetty server
 
-    protected CamelContext createCamelContext() throws Exception {
-        return camelContext
+  protected CamelContext createCamelContext() throws Exception {
+    return camelContext
+  }
+
+  @Before
+  void setUp(){
+    super.setUp()
+    camelContext.start() // starting camel ourselves
+    config = grailsApplication?.config
+  }
+
+  @After 
+  void tearDown(){
+    camelContext.stop() // stopping camel ourselves and after stop wiping out activemq queue
+
+    super.tearDown()
+  }
+
+  @Test
+  @DirtiesContext
+  void testProcessTika() {
+    camelContext.
+    getRouteDefinition('indexBinary').
+    adviceWith(camelContext,
+               new AdviceWithRouteBuilder() { 
+                 @Override
+                 public void configure() throws Exception {
+                   mockEndpointsAndSkip("direct:aggregate-documents")
+                 }
+               });
+
+    def pdfIs = new ClassPathResource('resources/2012-03-21-sunrise.pdf').inputStream
+    grailsApplication.mainContext.getBean('documentIndexer').transformations.put("http://www.abc.com", null)
+
+    assertNotNull(camelContext.hasEndpoint('mock:direct:aggregate-documents'))
+    def mockEndpoint = getMockEndpoint("mock:direct:aggregate-documents")
+
+    def expectation = { 
+      def ex = receivedExchanges[0]
+      assertEquals ex.in.body.documentElement.nodeName, 'doc'
+
+      assertEquals XPathAPI.selectSingleNode(ex.in.body, '//doc/field[@name="id"]/text()').nodeValue, 'http://www.abc.com'
+      assert XPathAPI.selectSingleNode(ex.in.body, '//doc/field[@name="title"]').textContent?.length() > 0
+      assert XPathAPI.selectSingleNode(ex.in.body, '//doc/field[@name="content"]').textContent?.length() > 0
     }
+    expectation.delegate = mockEndpoint
 
-    @Before
-    void setUp() {
-        super.setUp()
-        camelContext.start() // starting camel ourselves
-        config = grailsApplication?.config
-    }
+    mockEndpoint.expects(expectation)
 
-    @After
-    void tearDown() {
-        camelContext.stop() // stopping camel ourselves and after stop wiping out activemq queue
+    producerTemplate.sendBodyAndHeaders("direct:process-tika", pdfIs, [contextURI: 'http://www.abc.com'])
 
-        super.tearDown()
-    }
+    assertMockEndpointsSatisfied(config.gnutch.aggregationTime + 5, TimeUnit.SECONDS)
+  }
 
-    @Test
-    @DirtiesContext
-    void testProcessTika() {
-        camelContext.
-                getRouteDefinition('indexBinary').
-                adviceWith(camelContext,
-                        new AdviceWithRouteBuilder() {
-                            @Override
-                            public void configure() throws Exception {
-                                mockEndpointsAndSkip("direct:aggregate-documents")
-                            }
-                        });
+  @Test
+  @DirtiesContext
+  void testAggregate() {
+    camelContext.
+    getRouteDefinition('aggregation').
+    adviceWith(camelContext,
+               new AdviceWithRouteBuilder() { 
+                 @Override
+                 public void configure() throws Exception {
+                   mockEndpointsAndSkip("direct:publish")
+                 }
+               })
 
-        def pdfIs = new ClassPathResource('resources/2012-03-21-sunrise.pdf').inputStream
-        grailsApplication.mainContext.getBean('documentIndexer').transformations.put("http://www.abc.com", null)
-
-        assertNotNull(camelContext.hasEndpoint('mock:direct:aggregate-documents'))
-        def mockEndpoint = getMockEndpoint("mock:direct:aggregate-documents")
-
-        def expectation = {
-            def ex = receivedExchanges[0]
-            assertEquals ex.in.body.documentElement.nodeName, 'doc'
-
-            assertEquals XPathAPI.selectSingleNode(ex.in.body, '//doc/field[@name="id"]/text()').nodeValue, 'http://www.abc.com'
-            assert XPathAPI.selectSingleNode(ex.in.body, '//doc/field[@name="title"]').textContent?.length() > 0
-            assert XPathAPI.selectSingleNode(ex.in.body, '//doc/field[@name="content"]').textContent?.length() > 0
-        }
-        expectation.delegate = mockEndpoint
-
-        mockEndpoint.expects(expectation)
-
-        producerTemplate.sendBodyAndHeaders("direct:process-tika", pdfIs, [contextURI: 'http://www.abc.com'])
-
-        assertMockEndpointsSatisfied(config.gnutch.aggregationTime + 5, TimeUnit.SECONDS)
-    }
-
-    @Test
-    @DirtiesContext
-    void testAggregate() {
-        camelContext.
-                getRouteDefinition('aggregation').
-                adviceWith(camelContext,
-                        new AdviceWithRouteBuilder() {
-                            @Override
-                            public void configure() throws Exception {
-                                mockEndpointsAndSkip("direct:publish")
-                            }
-                        })
-
-        String[] docs = ['''<doc>
+    String [] docs = ['''<doc>
        <field name="title">foo</field>
        <field name="content">bar</field>
-      </doc>''',
-                '''<doc>
+      </doc>''', 
+                      '''<doc>
        <field name="title">foo1</field>
        <field name="content">bar1</field>
       </doc>''',
-                '''<doc>
+                      '''<doc>
        <field name="title">foo2</field>
        <field name="content">bar2</field>
       </doc>''']
 
-        assertNotNull(camelContext.hasEndpoint('mock:direct:publish'))
-        def mockEndpoint = getMockEndpoint("mock:direct:publish")
-        mockEndpoint.expectedMessageCount(1)
-        def expectation = {
-            def ex = receivedExchanges[0]
-            assert ex.in.body.documentElement.nodeName == 'add'
-            assert ex.in.body.getElementsByTagName('doc').length == 3
-        }
-        expectation.delegate = mockEndpoint
-        mockEndpoint.expects(expectation)
-        docs.each { doc ->
-            producerTemplate.sendBody("direct:aggregate-documents", doc)
-        }
-        assertMockEndpointsSatisfied(grailsApplication.config.gnutch.aggregationTime, TimeUnit.SECONDS)
+    assertNotNull(camelContext.hasEndpoint('mock:direct:publish'))
+    def mockEndpoint = getMockEndpoint("mock:direct:publish")
+
+    mockEndpoint.expectedMessageCount(1)
+    def expectation = { 
+      def ex = receivedExchanges[0]
+      assert ex.in.body.documentElement.nodeName == 'add'
+      assert ex.in.body.getElementsByTagName('doc').length == 3
     }
+    expectation.delegate = mockEndpoint
+    mockEndpoint.expects(expectation)
+    docs.each { doc ->
+      producerTemplate.sendBody("direct:aggregate-documents", doc) 
+    }
+
+//    assertMockEndpointsSatisfied(config.gnutch.aggregationTime + 5, TimeUnit.SECONDS)
+    assertMockEndpointsSatisfied(grailsApplication.config.gnutch.aggregationTime, TimeUnit.SECONDS)
+  }
+
 }
