45a1ed2b8994d6ba6fd4f9ab2b552a91db9435a1
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2015-03-21 09:20:32 +0200

    v0.2.3.2 drafted

--- GnutchGrailsPlugin.groovy
+++ GnutchGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import org.apache.camel.Exchange
 
 class GnutchGrailsPlugin {
 
-    def version = "0.2.3.1"
+    def version = "0.2.3.2"
     def grailsVersion = "2.2 > *"
     def loadAfter = ['controllers', 'services', 'routing']
     def title = "Grails Apache Nutch alternative"
